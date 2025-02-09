70a099abf322ba17bc809ebbfbaac008d05ebe3b
Jacob Delott <jldelott@gmail.com>
2025-02-09 10:39:19 -0800

    first commit success mind ar first try

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ARScene from "./components/ARScene";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="ar-container">
+      <ARScene />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ARScene.jsx
@@ -0,0 +1,77 @@
+import "mind-ar/dist/mindar-image.prod.js";
+import "aframe";
+import "mind-ar/dist/mindar-image-aframe.prod.js";
+import targetMind from "../assets/targets.mind?url";
+import videoFile from "../assets/BOP.mp4?url";
+import { useEffect } from "react";
+
+function ARScene() {
+  useEffect(() => {
+    const sceneEl = document.querySelector("a-scene");
+    const videoEl = document.querySelector("#bop-video");
+    const targetEl = document.querySelector("#target-container");
+
+    // AR system ready
+    sceneEl.addEventListener("arReady", () => {
+      console.log("AR is ready");
+    });
+
+    // AR system error
+    sceneEl.addEventListener("arError", () => {
+      console.log("AR failed to start");
+    });
+
+    // Target found/lost events
+    targetEl?.addEventListener("targetFound", () => {
+      console.log("target found");
+      videoEl.play();
+    });
+
+    targetEl?.addEventListener("targetLost", () => {
+      console.log("target lost");
+      videoEl.pause();
+    });
+
+    return () => {
+      targetEl?.removeEventListener("targetFound", () => {});
+      targetEl?.removeEventListener("targetLost", () => {});
+      sceneEl?.removeEventListener("arReady", () => {});
+      sceneEl?.removeEventListener("arError", () => {});
+    };
+  }, []);
+
+  return (
+    <a-scene
+      mindar-image={`imageTargetSrc: ${targetMind}; autoStart: true;`}
+      color-space="sRGB"
+      renderer="colorManagement: true, physicallyCorrectLights"
+      embedded
+      vr-mode-ui="enabled: false"
+      device-orientation-permission-ui="enabled: false"
+    >
+      <a-assets>
+        <video
+          id="bop-video"
+          src={videoFile}
+          preload="auto"
+          loop
+          crossOrigin="anonymous"
+          playsInline
+        ></video>
+      </a-assets>
+
+      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+      <a-entity id="target-container" mindar-image-target="targetIndex: 0">
+        <a-video
+          src="#bop-video"
+          position="0 0 0"
+          height="0.552"
+          width="1"
+          rotation="0 0 0"
+        ></a-video>
+      </a-entity>
+    </a-scene>
+  );
+}
+
+export default ARScene;
